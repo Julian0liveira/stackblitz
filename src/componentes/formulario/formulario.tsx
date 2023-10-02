cc2e4b319f45803943974a930858d38a84cf06c7
Julian0liveira <143848613+Julian0liveira@users.noreply.github.com>
2023-10-02 20:47:17 -0300

    1 file modified

--- src/componentes/formulario/formulario.tsx
+++ src/componentes/formulario/formulario.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
 export default function Formulario(){
-  
+  return(
+    <div>
+      <form>
+        <label> htmlfor='tarefa' </label>
+      </form>
+    </div>
+  )
 }
