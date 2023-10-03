a9f0c8527c740d3e10b212e647cc8511a7acd0f9
Julian0liveira <143848613+Julian0liveira@users.noreply.github.com>
2023-10-02 21:22:26 -0300

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import Formulario from './componentes/formulario/formulario';
 import './style.css';
 //import Formulario from './componentes/formulario/formulario';
 
@@ -6,10 +7,8 @@ export default function App() {
   return (
     <>
       <div>
-        <h1>Hello</h1>
-        <p>Start editing to see some magic happen :)</p>
+        <Formulario />
       </div>
-      <div></div>
     </>
   );
 }
