ef650c05b4aa7504c1394390147bb2f44120c60b
Julian0liveira <143848613+Julian0liveira@users.noreply.github.com>
2023-10-02 21:03:24 -0300

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
-
 import './style.css';
+import Formulario from './componentes/formulario/formulario';
 
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
-    <div>
-      <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+      <div>
+        <Formulario />
+      </div>
+    </>
   );
 };
