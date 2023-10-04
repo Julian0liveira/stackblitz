80d09c60960b8b7fbd99433b487b43367a731662
Julian0liveira <143848613+Julian0liveira@users.noreply.github.com>
2023-10-04 09:24:01 -0300

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,19 @@ export default function App() {
       <h1> Cadastro de aluno </h1>
     </div>
     <div>
-      <label htmlFor=""> Nome completo </label>
+      <label htmlFor="nome"> Nome completo </label>
     </div>
     <div>      
       <input type="text" name="nome" id="nome" placeholder="Digite seu nome completo" required/>
     </div>
     <div>
-      <label htmlFor=""> Data de nascimento </label>
+      <label htmlFor="datanascimento"> Data de nascimento </label>
     </div>
     <div>
       <input type="date" name="data" id="datanascimento" placeholder="Digite sua data de nascimento" required />
     </div>
     <div>
-    <label htmlFor=""> Genero </label>
+    <label htmlFor="genero"> Genero </label>
       <select id="genero">
       <option value="selecione" defaultChecked>Selecionar</option>
       <option value="masculino">Masculino</option>
@@ -31,13 +31,13 @@ export default function App() {
       </select>
     </div>
     <div>
-      <label htmlFor=""> Nome da mãe </label>
+      <label htmlFor="nomemae"> Nome da mãe </label>
     </div>
     <div>
       <input type="text" name="nomemae" id="nomemae" placeholder="Digite o nome de sua mãe" />
     </div>
     <div>
-      <label htmlFor=""> Nome do pai </label>
+      <label htmlFor="nomepai"> Nome do pai </label>
     </div>
     <div>
       <input type="text" name="nomepai" id="nomepai" placeholder="Digite o nome de seu pai" />
