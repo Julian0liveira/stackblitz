a6e1f544836ae98301df87ecb92b8fac449aa151
Julian0liveira <143848613+Julian0liveira@users.noreply.github.com>
2023-10-03 22:06:30 -0300

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import { FC } from 'react';
-import Formulario from './componentes/formulario/formulario';
+import Botao from './componentes/formulario/botao';
 import './style.css';
 //import Formulario from './componentes/formulario/formulario';
 
 export default function App() {
   return (
     <>
-      <div>
-        <Formulario />
-      </div>
+    <form>
+    <div>
+      <h1> Cadastro de aluno </h1>
+    </div>
+    <div>
+      <label htmlFor=""> Nome completo </label>      
+      <input type="text" name="nome" id="nome" placeholder="Digite seu nome completo" required/>
+    </div>
+      <label htmlFor=""> Data de nascimento </label>
+      <input type="date" name="data" id="datanascimento" placeholder="Digite sua data de nascimento" required />
+    <div>
+      <label htmlFor=""> Genero </label>
+    </div>
+    <div>
+      <label>
+      <input type="radio" name="genero" id="genero" value="masculino" checked/>Masculino
+      </label>
+    </div>
+    <div>
+      <label>
+      <input type="radio" name="genero" id="genero" value="feminino"/>Feminino
+      </label>
+    </div>
+      <Botao/>
+  </form>
     </>
   );
 }
